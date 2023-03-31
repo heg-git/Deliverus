1e911db1e7afcde90219c41af7aa8822b19eae0b
heg-git <ghddmsrl1@naver.com>
2023-04-01 00:33:04 +0900

    프로젝트 생성 및 다이어그램 추가

---
+++ deliverus/src/main/java/kau/coop/deliverus/DeliverusApplication.java
@@ -0,0 +1,13 @@
+package kau.coop.deliverus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverusApplication.class, args);
+	}
+
+}
---
+++ deliverus/src/test/java/kau/coop/deliverus/DeliverusApplicationTests.java
@@ -0,0 +1,13 @@
+package kau.coop.deliverus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
