4c7c180bc23f071f33cc58ee7fe3e64946c3d331
hhtboy <hhtboy1024@gmail.com>
2023-04-07 21:43:57 +0900

    세션 직접 구현

--- deliverus/src/main/java/kau/coop/deliverus/controller/login/LoginController.java
+++ deliverus/src/main/java/kau/coop/deliverus/controller/login/LoginController.java
@@ -1,17 +1,60 @@
 package kau.coop.deliverus.controller.login;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import kau.coop.deliverus.domain.UserInfo;
+import kau.coop.deliverus.domain.dto.LoginFormDto;
+import kau.coop.deliverus.domain.session.SessionManager;
 import kau.coop.deliverus.service.login.LoginService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class LoginController {
 
-    private LoginService loginService;
+    private final LoginService loginService;
+    private final SessionManager sessionManager;
 
+    @GetMapping("/member/login")
+    public String test(HttpServletRequest request, HttpServletResponse response) {
+        Object session = sessionManager.getSession(request);
+        if(session == null) {
+            try {
+                response.sendRedirect("/member/login/loginForm");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else{
+            // 세션 정보가 있다면
+            log.info("login 되어있음");
+        }
+        return "hello";
+    }
 
-
+    @GetMapping("/member/login/loginForm")
+    public String loginForm() {
+        return "login form";
+    }
+    @PostMapping("/member/login/loginForm")
+    public String loginProc(@RequestBody LoginFormDto login, BindingResult bindingResult, HttpServletResponse response) {
+        try {
+            loginService.login(login.getUserid(), login.getPasswd());
+            UserInfo userInfo = new UserInfo("hhtboy", 24);
+            sessionManager.createSession(userInfo, response);
+            return "login success!";
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            bindingResult.reject(e.getMessage());
+            return "login failed..";
+        }
+    }
 }
