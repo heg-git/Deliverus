8434bdd6ea3c16f12904f370c1ebeabaa1edb625
hhtboy <hhtboy1024@gmail.com>
2023-05-19 14:25:39 +0900

    불필요 코드 삭제

--- deliverus/src/main/java/kau/coop/deliverus/controller/healthcheck/HealthcheckController.java
+++ deliverus/src/main/java/kau/coop/deliverus/controller/healthcheck/HealthcheckController.java
@@ -1,16 +1,10 @@
 package kau.coop.deliverus.controller.healthcheck;
 
-import kau.coop.deliverus.service.lambda.ApiServiceImpl;
-import kau.coop.deliverus.service.lambda.LambdaTask;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class HealthcheckController {
-
-    private final LambdaTask lambdaTask;
 
     @GetMapping("api/healthcheck")
     public String heathCheck() {
