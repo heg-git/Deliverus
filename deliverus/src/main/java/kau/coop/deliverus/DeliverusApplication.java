c3b476b3a5bd88c572ff01ff91cba069762bdd9b
hhtboy <hhtboy1024@gmail.com>
2023-05-19 14:21:44 +0900

    lambda api 호출을 위한 외부 api 받아오기 구현

--- deliverus/src/main/java/kau/coop/deliverus/DeliverusApplication.java
+++ deliverus/src/main/java/kau/coop/deliverus/DeliverusApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class DeliverusApplication {
 
 	public static void main(String[] args) {
